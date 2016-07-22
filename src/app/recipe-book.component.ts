7c4c6224f8e794c190960ee8f93bc9740b50e896
insta-code1 <eoinkane1@gmail.com>
2016-07-22 01:27:03 +0100

    Added Shopping list component & Recipe Detail comp & Databinding

--- src/app/recipe-book.component.ts
+++ src/app/recipe-book.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './header.component';
 import { RecipesComponent } from './recipes';
+import { ShoppingListComponent } from './shopping-list';
 
 @Component({
   moduleId: module.id,
   selector: 'rb-recipe-book',
   templateUrl: 'recipe-book.component.html',
-  directives: [HeaderComponent, RecipesComponent]
+  directives: [HeaderComponent, RecipesComponent, ShoppingListComponent]
 })
 export class RecipeBookComponent {
 
